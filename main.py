2e872510122cae361ac2731f6c6fd6b3650e2442
erostrate9 <erostrate9@foxmail.com>
2021-10-30 00:56:40 +0800

    length finished

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+def LCIS(S, T):
+    m = len(S)
+    n = len(S)
+    dp = [0] * m
+    prev = [[-1] * n] * m
+    lcis = [''] * (min(m, n))
+    for i in range(m):
+        # l stands for the length of LCIS
+        # p stands for the end of LCIS
+        # p must < s[i]
+        l, p = 0, -1
+        for j in range(n):
+            if S[i] == T[j]:
+                if dp[j] < l + 1:
+                    dp[j] = l + 1
+                    prev[i][j] = p
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+            if T[j] < S[i]:
+                if dp[j] > l:
+                    l = dp[j]
+                    p = j
+    l, p = 0, -1
+    for i in range(n):
+        if (dp[i] > l):
+            l = dp[i]
+            p = i
+    j, k = p, l
+    for i in range(m - 1, -1, -1):
+        if prev[i][j] != -1:
+            k -= 1
+            lcis[k] = T[j]
+            j = prev[i][j]
+        if k <= 0:
+            break
+    return l, lcis
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def __main__():
+    S = [1, 2, 4, 3, 4]
+    T = [4, 2, 1, 4, 2, 3, 1]
+    l, lcis = LCIS(S, T)
+    print("Length of LCIS is", l)
+    print("LCIS is:")
+    for i in range(l):
+        if lcis[i] != '':
+            print(lcis[i], end=',')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    __main__()
